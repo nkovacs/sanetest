ac9445d241f29b944aa39846456c806cd64e9f72
Nikola Kovacs <nikola.kovacs@gmail.com>
2016-07-12 16:14:43 +0200

    improve output

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var sane=require('sane');
 
 var watcher = sane('test/', {glob: ['**/*.txt']});
 watcher.on('ready', function () { console.log('ready') });
-watcher.on('change', function (filepath, root, stat) { console.log('file changed', filepath); });
-watcher.on('add', function (filepath, root, stat) { console.log('file added', filepath); });
-watcher.on('delete', function (filepath, root) { console.log('file deleted', filepath); });
+watcher.on('change', function (filepath, root, stat) { console.log('\tfile changed', filepath); });
+watcher.on('add', function (filepath, root, stat) { console.log('\tfile added', filepath); });
+watcher.on('delete', function (filepath, root) { console.log('\tfile deleted', filepath); });
